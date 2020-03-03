95b32dfe4a14081f1c396bcddd61912760d1d46f
kjdowns <kevin.downs04@gmail.com>
2020-03-03 15:51:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Add your functions here
+
+function map(array, func) {
+    let mapArray =[];
+    for (let i = 0; i < array.length; i++) {
+       mapArray.push(func(array[i]));
+    }
+    return mapArray;
+}
+
+function reduce(array, func, start=0) {
+    if (typeof(func(array[0], start)) === "boolean"){
+		start = true
+	}
+    for (let i = 0; i < array.length; i++) {
+        start = func(array[i],start);
+    }
+    return start;
+}
